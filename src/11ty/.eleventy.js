64bf624fe4546706f80bd4a7a900476b94ff6926
wommy <tom@technomad.media>
2021-04-21 20:53:51 -0400

    _component/menu-starters

---
+++ src/11ty/.eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function(config) {
+	config.addPassthroughCopy({ "static" : "/" })
+	config.addShortcode("menuItem", require('./11ty/_components/menuItem.11ty.js')
+)
+	return {
+		dir: {
+			input: "11ty",
+			includes: "_components",
+		},
+		htmlTemplateEngine: "njk",
+	};
+};
---
+++ src/11ty/_components/menuItem.11ty.js
@@ -0,0 +1,6 @@
+module.exports = ({ name, price }) => `
+<div class="gap-2.5 flex justify-end tracking-tight">
+	<span>${ name }</span>
+	<span class="w-8">$ ${ price }</span>
+</div>
+`

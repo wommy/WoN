05ff044f433e01ccc90dca58f49701d766a9a858
wommy <tom@technomad.media>
2021-04-21 21:25:24 -0400

    _data/starters

---
+++ src/11ty/_data/starters.js
@@ -0,0 +1,48 @@
+[
+	{
+		name: 'hardshell taco plate',
+		price: 9,
+	},{
+		name: 'cheesy beef tots',
+		price: 12,
+	},{ 
+		name:'dynamite tots', 
+		price: 13,
+	},{ 
+		name:'mini angus burger', 
+		price: 12,
+	},{ 
+		name:'*wings, 6', 
+		price: 9,
+	},{ 
+		name:'*wings, 12', 
+		price: 16,
+	},{ 
+		name:'cheddar cubes', 
+		price: 8,
+	},{ 
+		name:'cajun curds', 
+		price: 8,
+	},{ 
+		name:'cheese fries', 
+		price: 6,
+	},{ 
+		name:'chicken fingers', 
+		price: 8,
+	},{ 
+		name:'deep fried pickles', 
+		price: 8,
+	},{ 
+		name:'quesadilla', 
+		price: 7.5,
+	},{ 
+		name:'chickendilla', 
+		price: 12,
+	},{ 
+		name:'basket of fries/tots', 
+		price: 5,
+	},{ 
+		name:'basket of chips', 
+		price: 4,
+	}
+]

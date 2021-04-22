bf92c576a5482624230d29efaa8b9bcfa8bbb027
wommy <tom@technomad.media>
2021-04-22 00:46:59 -0400

    specials

---
+++ src/11ty/_data/specials.js
@@ -0,0 +1,27 @@
+module.exports = [
+	{
+		day: 'happy hour (mon-fri 4-6pm)',
+		special: '$2 miller drafts & $.50 tacos (min 4)',
+	},{
+		day: 'monday',
+		special: '$7 italian beef',
+	},{
+		day: 'tuesday',
+		special: '$7 menu pizza'
+	},{
+		day: 'wednesday',
+		special: '$7 burgers',
+	},{
+		day: 'thursday',
+		special: '$10 1/2 slab'
+	},{
+		day: 'friday',
+		special: '$7 fish & chips',
+	},{
+		day: 'saturday',
+		special: '$1.50 sliders',
+	},{
+		day: 'sunday',
+		special: '1/2 price wings'
+	}
+]
